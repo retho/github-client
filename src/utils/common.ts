a4ff4779302c23125d75bef249600617cb4d0b07
retho <safonov.e.s@gmail.com>
2020-04-10 20:07:58 +0300

    useForceUpdate

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,10 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useReducer } from 'react';
 
 export const assertNever = (val: never, throwErr = false) => {
   if (throwErr) throw new Error(`Never error: ${val}`);
 };
 
 export const stopPropagation = (e: SyntheticEvent) => e.stopPropagation();
+
+export const useForceUpdate = () =>
+  useReducer((s) => s + 1, 0)[1] as () => void;
