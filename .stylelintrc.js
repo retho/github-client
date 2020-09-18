4b9059e081c74861a11eb384b4cb111bbd94e078
Safonov Evgeny <safonov.e.s@gmail.com>
2020-09-18 13:27:23 +0300

    -

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,5 +4,7 @@ module.exports = {
     'at-rule-no-unknown': null,
     'custom-property-empty-line-before': null,
     'no-empty-source': null,
+    'value-list-comma-newline-after': null,
+    'declaration-colon-newline-after': null,
   },
 };
